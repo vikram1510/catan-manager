c1cc743864ce67ee73297076848aa91d4ea7be0e
bagejar <rahul.bageja@ig.com>
2020-05-17 12:16:57 +0100

    simplified filtering player from players

--- src/components/PlayerCards.js
+++ src/components/PlayerCards.js
@@ -6,23 +6,21 @@ import PlayerCard from '../components/PlayerCard'
 const PlayerCards = ({ players, player: mainPlayer, updatePlayers }) => {
 
 	const [tradePlayerId, setTradePlayerId] = useState(null)
+	const otherPlayers = players.filter((pl) => pl._id !== mainPlayer._id)
 
 	return (
-		players.map((opp, key) =>
-			(opp._id !== mainPlayer._id) ?
-				<PlayerCardWrapper key={key}>
-					<PlayerCard
-						mainPlayer={mainPlayer}
-						player={opp}
-						showTrade={opp._id === tradePlayerId}
-						setTradePlayerId={setTradePlayerId}
-						updatePlayers={updatePlayers}
-					/>
-				</PlayerCardWrapper>
-				: undefined)
+		otherPlayers.map((player) =>
+			<PlayerCardWrapper key={player._id}>
+				<PlayerCard
+					player={player}
+					mainPlayer={mainPlayer}
+					showTrade={player._id === tradePlayerId}
+					setTradePlayerId={setTradePlayerId}
+					updatePlayers={updatePlayers}
+				/>
+			</PlayerCardWrapper>)
 	)
 }
-
 
 const PlayerCardWrapper = styled.div`
 margin-bottom: 10px;
