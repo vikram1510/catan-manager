08fc6de8b364333be596d4eb783d47ef5b66fd85
Vikram Bageja <vikram.bageja@and.digital>
2022-12-08 22:02:10 +0000

    Change path

--- src/lib/assets.js
+++ src/lib/assets.js
@@ -1,26 +1,26 @@
-const { PUBLIC_URL } = process.env
+const { PUBLIC_URL } = process.env;
 
-const RESOURCE_FOLDER = PUBLIC_URL + '/resource/'
-const HARBOUR_FOLDER = PUBLIC_URL + '/harbour/'
+const RESOURCE_FOLDER = PUBLIC_URL + "/resource";
+const HARBOUR_FOLDER = PUBLIC_URL + "/harbour";
 
 const assets = {
-  brick: RESOURCE_FOLDER + '/NewBricksCatan.png',
-  sheep: RESOURCE_FOLDER + '/NewSheepCatan.png',
-  wood: RESOURCE_FOLDER + '/NewTreeCatan.png',
-  grain: RESOURCE_FOLDER + '/NewWheatCatan.png',
-  rock: RESOURCE_FOLDER + '/NewRocksCatan.png',
-  bg: PUBLIC_URL + '/bg.jpg',
-  logo: PUBLIC_URL + '/catanlogo.png',
-}
+  brick: RESOURCE_FOLDER + "/NewBricksCatan.png",
+  sheep: RESOURCE_FOLDER + "/NewSheepCatan.png",
+  wood: RESOURCE_FOLDER + "/NewTreeCatan.png",
+  grain: RESOURCE_FOLDER + "/NewWheatCatan.png",
+  rock: RESOURCE_FOLDER + "/NewRocksCatan.png",
+  bg: PUBLIC_URL + "/bg.jpg",
+  logo: PUBLIC_URL + "/catanlogo.png",
+};
 
 export const harborAssets = {
-  brick: HARBOUR_FOLDER + '/harbour_brick.png',
-  sheep: HARBOUR_FOLDER + '/harbour_sheep.png',
-  wood: HARBOUR_FOLDER + '/harbour_wood.png',
-  grain: HARBOUR_FOLDER + '/harbour_grain.png',
-  rock: HARBOUR_FOLDER + '/harbour_rock.png',
-  "any-3": HARBOUR_FOLDER + '/harbour_any-3.png',
-  "any-4": HARBOUR_FOLDER + '/harbour_any-4.png',
-}
+  brick: HARBOUR_FOLDER + "/harbour_brick.png",
+  sheep: HARBOUR_FOLDER + "/harbour_sheep.png",
+  wood: HARBOUR_FOLDER + "/harbour_wood.png",
+  grain: HARBOUR_FOLDER + "/harbour_grain.png",
+  rock: HARBOUR_FOLDER + "/harbour_rock.png",
+  "any-3": HARBOUR_FOLDER + "/harbour_any-3.png",
+  "any-4": HARBOUR_FOLDER + "/harbour_any-4.png",
+};
 
-export default assets
+export default assets;
