be5353a207c7a7f921e03c270484c77ced3efff1
Vikram Bageja <vikram.bageja@and.digital>
2022-12-08 22:09:29 +0000

    Use HashRouter

--- src/App.js
+++ src/App.js
@@ -1,27 +1,38 @@
-import React, { useState } from 'react';
-import styled from 'styled-components'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import Game from './pages/Game'
-import Login from './pages/Login'
-import assets from './lib/assets';
-import colors from './lib/colors'
+import React, { useState } from "react";
+import styled from "styled-components";
+import { Switch, Route, HashRouter } from "react-router-dom";
+import Game from "./pages/Game";
+import Login from "./pages/Login";
+import assets from "./lib/assets";
+import colors from "./lib/colors";
 
 function App() {
-
   const [theme, setTheme] = useState("dark");
 
   const toggleTheme = () => {
-    theme === 'light' ? setTheme("dark") : setTheme("light")
-  }
+    theme === "light" ? setTheme("dark") : setTheme("light");
+  };
 
   return (
     <AppScreen className="App" bg={assets.bg} theme={theme}>
-      <BrowserRouter>
+      <HashRouter>
         <Switch>
-          <Route exact path='/' render={(props) => <Login {...props} theme={theme} toggleTheme={toggleTheme} />} />
-          <Route exact path='/game' render={(props) => <Game {...props} theme={theme} toggleTheme={toggleTheme} />} />
+          <Route
+            exact
+            path="/"
+            render={(props) => (
+              <Login {...props} theme={theme} toggleTheme={toggleTheme} />
+            )}
+          />
+          <Route
+            exact
+            path="/game"
+            render={(props) => (
+              <Game {...props} theme={theme} toggleTheme={toggleTheme} />
+            )}
+          />
         </Switch>
-      </BrowserRouter>
+      </HashRouter>
     </AppScreen>
   );
 }
@@ -29,11 +40,11 @@ function App() {
 const AppScreen = styled.div`
 width:100vw;
 height:100vh;
-background-color:${props => colors[props.theme].background};
+background-color:${(props) => colors[props.theme].background};
 font-family: 'Baloo 2';
-background-image: url(${({ theme, bg }) => theme === "light" ? bg : ""});
+background-image: url(${({ theme, bg }) => (theme === "light" ? bg : "")});
 overflow: scroll;
 background - position: center;
-`
+`;
 
 export default App;
