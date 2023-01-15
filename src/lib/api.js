b2862cb9f7ccb26e78134f16ddc65e708f60540f
Vikram Bageja <vikram.bageja@and.digital>
2023-01-15 20:24:28 +0000

    Fix api url

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,192 +1,168 @@
-import axios from 'axios'
-import { resourceArray } from './config'
+import axios from "axios";
+import { resourceArray } from "./config";
 
-let baseURL
+let baseURL;
 
-if (process.env.REACT_APP_API_VERSION === 'test') {
-  baseURL = 'http://192.168.0.41:3030'
+if (process.env.REACT_APP_API_VERSION === "test") {
+  baseURL = "http://192.168.0.41:3030";
 } else {
-  baseURL = 'https://catan-manager-api-rb.herokuapp.com/'
+  baseURL = "http://apicatan-env.eba-nps4phg8.eu-west-1.elasticbeanstalk.com/";
 }
 
-console.log('Connecting to database', baseURL);
+console.log("Connecting to database", baseURL);
 
-let instance = axios.create({ baseURL })
+let instance = axios.create({ baseURL });
 
 const getAllPlayers = () => {
-
   const response = instance
-    .get('/players')
-    .then(res => res.data)
-    .catch(err => console.log('Error in getting data', err.response.data))
+    .get("/players")
+    .then((res) => res.data)
+    .catch((err) => console.log("Error in getting data", err.response.data));
 
-  return response
-}
+  return response;
+};
 
 const getPlayerByName = (name) => {
-
   const response = instance
-    .get('/players?name=' + name)
-    .then(res => {
+    .get("/players?name=" + name)
+    .then((res) => {
       if (res.data.length === 1) {
-        return res.data[0] ?? undefined
+        return res.data[0] ?? undefined;
       }
     })
-    .catch(err => console.log('Error in getting player by name ' + name, err.response.data))
+    .catch((err) =>
+      console.log("Error in getting player by name " + name, err.response.data)
+    );
 
-  return response
-}
-
+  return response;
+};
 
 const getPlayerByID = (id) => {
-
   const response = instance
-    .get('/players/' + id)
-    .then(res => {
+    .get("/players/" + id)
+    .then((res) => {
       if (res.data) {
-        return res.data
+        return res.data;
       }
-      console.log('Player with id: ' + id + ' does not exist')
+      console.log("Player with id: " + id + " does not exist");
     })
-    .catch(err => console.log('Error in getting player by id ' + id, err))
+    .catch((err) => console.log("Error in getting player by id " + id, err));
 
-  return response
-}
+  return response;
+};
 
 const createPlayer = (name) => {
-
   const response = instance
-    .post('/players/', { name })
-    .then(res => res.data._id)
-    .catch(err => console.log('Error in creating player: ' + name, err.response.data))
+    .post("/players/", { name })
+    .then((res) => res.data._id)
+    .catch((err) =>
+      console.log("Error in creating player: " + name, err.response.data)
+    );
 
-  return response
-}
+  return response;
+};
 
 const deletePlayer = (id) => {
-
   instance
-    .delete('/players/' + id)
-    .then(_ => console.log('Successfully deleted player: ' + id))
-    .catch(err => console.log('Error in deleting player', err.response.data))
-
-}
+    .delete("/players/" + id)
+    .then((_) => console.log("Successfully deleted player: " + id))
+    .catch((err) => console.log("Error in deleting player", err.response.data));
+};
 
 const updatePlayer = (id, payload) => {
-
   return instance
-    .put('/players/' + id, payload)
-    .then(res => res.data)
-    .catch(err => console.log('Error in updating player: ' + id + ' with: ' + payload, err.response.data))
-}
+    .put("/players/" + id, payload)
+    .then((res) => res.data)
+    .catch((err) =>
+      console.log(
+        "Error in updating player: " + id + " with: " + payload,
+        err.response.data
+      )
+    );
+};
 
 const transaction = ({ fromId, toId, amounts }) => {
-
-  let verifiedAmounts = {}
+  let verifiedAmounts = {};
 
   resourceArray.forEach((resource) => {
     if (amounts?.[resource]) {
-      verifiedAmounts[resource] = amounts[resource]
+      verifiedAmounts[resource] = amounts[resource];
     }
-  })
+  });
 
   return instance
-    .post('/players/transaction', { fromId, toId, amounts: verifiedAmounts })
-    .then(res => res.data)
-    .catch(err => console.log('Error in performing transaction', fromId, toId, amounts))
-}
+    .post("/players/transaction", { fromId, toId, amounts: verifiedAmounts })
+    .then((res) => res.data)
+    .catch((err) =>
+      console.log("Error in performing transaction", fromId, toId, amounts)
+    );
+};
 
 const bank = ({ amounts, playerId }) => {
-
-  console.log(amounts, playerId)
+  console.log(amounts, playerId);
   return instance
-    .post('/players/bank', { playerId, amounts })
-    .then(res => res.data)
-    .catch(err => console.log('Error in performing banking'))
-}
-
+    .post("/players/bank", { playerId, amounts })
+    .then((res) => res.data)
+    .catch((err) => console.log("Error in performing banking"));
+};
 
 const getHistory = () => {
-
-
   return instance
-    .get('/history')
-    .then(res => res.data)
-    .catch(err => console.log('Error in getting history', err))
-
-}
-
+    .get("/history")
+    .then((res) => res.data)
+    .catch((err) => console.log("Error in getting history", err));
+};
 
 const addToHistory = ({ text, type }) => {
-
   return instance
-    .post('/history', { text, type })
-    .then(res => res.data)
-    .catch(err => console.log('Error in updating history', err))
-
-}
-
+    .post("/history", { text, type })
+    .then((res) => res.data)
+    .catch((err) => console.log("Error in updating history", err));
+};
 
 const deleteHistory = () => {
-
   instance
-    .delete('/history')
-    .then(res => res.data)
-    .catch(err => console.log('Error in deleting history', err))
-
-}
-
+    .delete("/history")
+    .then((res) => res.data)
+    .catch((err) => console.log("Error in deleting history", err));
+};
 
 const getEvents = () => {
-
-
   return instance
-    .get('/events')
-    .then(res => res.data)
-    .catch(err => console.log('Error in getting Events', err))
-
-}
-
+    .get("/events")
+    .then((res) => res.data)
+    .catch((err) => console.log("Error in getting Events", err));
+};
 
 const addToEvents = ({ name, createdBy }) => {
-
   return instance
-    .post('/events', { name, createdBy })
-    .then(res => res.data)
-    .catch(err => console.log('Error in updating Events', err))
-
-}
-
+    .post("/events", { name, createdBy })
+    .then((res) => res.data)
+    .catch((err) => console.log("Error in updating Events", err));
+};
 
 const deleteEvents = () => {
-
   return instance
-    .delete('/events')
-    .then(res => res.data)
-    .catch(err => console.log('Error in deleting Events', err))
-
-}
-
+    .delete("/events")
+    .then((res) => res.data)
+    .catch((err) => console.log("Error in deleting Events", err));
+};
 
 const addToRobSheet = ({ robber, innocent, robbedItem }) => {
-
   return instance
-    .post('/rob', { values: [robber, innocent, robbedItem] })
-    .then(res => res.data)
-    .catch(err => console.log('Error in adding rob event', err))
-
-}
+    .post("/rob", { values: [robber, innocent, robbedItem] })
+    .then((res) => res.data)
+    .catch((err) => console.log("Error in adding rob event", err));
+};
 
 const newRobSheet = () => {
-
-  let date = new Date().toLocaleString()
+  let date = new Date().toLocaleString();
 
   return instance
-    .post('/rob', { values: ["NEW GAME", date] })
-    .then(res => res.data)
-    .catch(err => console.log('Error in adding rob event', err))
-
-}
+    .post("/rob", { values: ["NEW GAME", date] })
+    .then((res) => res.data)
+    .catch((err) => console.log("Error in adding rob event", err));
+};
 
 const api = {
   getAllPlayers,
@@ -204,9 +180,7 @@ const api = {
   addToEvents,
   deleteEvents,
   addToRobSheet,
-  newRobSheet
-}
+  newRobSheet,
+};
 
-export default api
-
-
+export default api;
